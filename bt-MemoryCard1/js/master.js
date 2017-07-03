85f2cb25b476a8c20e6d340beae0f6e69dc83231
hungp196 <hungp12897@gmail.com>
2017-07-03 23:29:48 +0700

    MemoryCard-3

--- bt-MemoryCard1/js/master.js
+++ bt-MemoryCard1/js/master.js
@@ -1,6 +1,6 @@
-//document.getElementById("theme-music").play();
-var cards = ['f1','f2','f3','f4','f5','f6','f7','f8','f9','f10','f11','f12'];
+var cards = ['f1','f2','f3'];
 var current = null;
+var sum = 0;
 
 function flip(card) {
   document.getElementById("choise-music").load();
@@ -12,12 +12,11 @@ function flip(card) {
     current.css('pointer-events', 'none');
   }else{
     if(current.attr('data-name') != $(card).attr('data-name')){
-      setTimeout(function() {
+      setTimeout(function(){
         current.toggleClass('flipped');
         $(card).toggleClass('flipped');
-          current = null;
-      }, 300);
-
+        current = null;
+      },200);
     }else {
       document.getElementById("bg-music").load();
       document.getElementById("bg-music").play();
@@ -25,7 +24,8 @@ function flip(card) {
         current.css('opacity','0');
         $(card).css('opacity','0');
         current = null;
-      },200);
+      },100);
+      sum++;
     }
     current.css('pointer-events', 'auto');
   }
@@ -52,16 +52,54 @@ function shuffle(array) {
   return array;
 }
 
+
+
 $(function() {
-  cards = cards.concat(cards);
-  shuffle(cards);
-  var html = '';
-  for (var i = 0; i < cards.length; i++) {
-    html += '<div class="grid">'+
-    '<div class="card" data-name="'+cards[i]+'" onclick="flip(this)">'+
-    '<div class="front"><img src="img/'+cards[i]+'.jpg" alt=""></div>'+
-    '<div class="back"><img src="img/back.jpg" alt=""></div>'+
-    '</div></div>';
-  }
-  $('.content').html(html);
+  swal({
+    title: "Are you ready?",
+    text: "Try the best",
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "Yes",
+    cancelButtonText: "No",
+    closeOnConfirm: true,
+    closeOnCancel: false
+  },
+  function(isConfirm){
+    if (isConfirm) {
+      //document.getElementById("theme-music").play();
+      cards = cards.concat(cards);
+      shuffle(cards);
+      var html = '';
+      for (var i = 0; i < cards.length; i++) {
+        html += '<div class="grid">'+
+        '<div class="card" data-name="'+cards[i]+'" onclick="flip(this)">'+
+        '<div class="front"><img src="img/'+cards[i]+'.jpg" alt=""></div>'+
+        '<div class="back"><img src="img/back.jpg" alt=""></div>'+
+        '</div></div>';
+      }
+      $('.content').html(html);
+      var elem = document.getElementById("myBar");
+      var width = 1;
+      var id = setInterval(frame, 100);
+      function frame() {
+          if (width >= 100) {
+              //clearInterval(id);
+              swal("Game Over!!!");
+          } else {
+              if(sum<3){
+                width++;
+                elem.style.width = width + '%';
+              }else {
+                clearInterval(id);
+                swal("Good Game!!!");
+              }
+          }
+      }
+    }else {
+      swal("Start Again Please!");
+    }
+  });
+  //document.getElementById("theme-music").play();
 });
