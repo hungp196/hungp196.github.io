9665b07ca630788e233efab7384940d99cc26984
hungp196 <hungp12897@gmail.com>
2017-07-03 15:10:08 +0700

    bt-MemoryCard2

--- bt-MemoryCard1/js/master.js
+++ bt-MemoryCard1/js/master.js
@@ -1,8 +1,36 @@
-var cards = ['f1','f2','f3','f4','f5'];
+//document.getElementById("theme-music").play();
+var cards = ['f1','f2','f3','f4','f5','f6','f7','f8','f9','f10','f11','f12'];
+var current = null;
 
 function flip(card) {
+  document.getElementById("choise-music").load();
+  document.getElementById("choise-music").play();
   $(card).toggleClass('flipped');
-  $(card).css('pointer-events', 'none');
+
+  if(!current){
+    current = $(card);
+    current.css('pointer-events', 'none');
+  }else{
+    if(current.attr('data-name') != $(card).attr('data-name')){
+      setTimeout(function() {
+        current.toggleClass('flipped');
+        $(card).toggleClass('flipped');
+          current = null;
+      }, 300);
+
+    }else {
+      document.getElementById("bg-music").load();
+      document.getElementById("bg-music").play();
+      setTimeout(function(){
+        current.css('opacity','0');
+        $(card).css('opacity','0');
+        current = null;
+      },200);
+    }
+    current.css('pointer-events', 'auto');
+  }
+
+
 }
 
 function shuffle(array) {
@@ -30,7 +58,7 @@ $(function() {
   var html = '';
   for (var i = 0; i < cards.length; i++) {
     html += '<div class="grid">'+
-    '<div class="card" onclick="flip(this)">'+
+    '<div class="card" data-name="'+cards[i]+'" onclick="flip(this)">'+
     '<div class="front"><img src="img/'+cards[i]+'.jpg" alt=""></div>'+
     '<div class="back"><img src="img/back.jpg" alt=""></div>'+
     '</div></div>';
